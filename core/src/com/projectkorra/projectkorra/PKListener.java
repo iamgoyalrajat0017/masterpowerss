e56239d8369b62a438dd5fbaf3a0928ffa70cf86
Sc1original <bardiasarlak@hotmail.com>
2026-06-27 21:28:21 +0330

    Added heatcontrol mob extinguish feature

--- core/src/com/projectkorra/projectkorra/PKListener.java
+++ core/src/com/projectkorra/projectkorra/PKListener.java
@@ -1323,6 +1323,16 @@ public class PKListener implements Listener {
 			BlockSource.update(player, ClickType.SHIFT_DOWN);
 		}
 
+		Entity entity = GeneralMethods.getTargetedEntity(player, 3.5);
+
+		if(bPlayer.canBend(CoreAbility.getAbility(HeatControl.class))){
+
+			if(entity.getFireTicks() > 0){
+				entity.setFireTicks(0);
+			}
+
+		}
+
 		final CoreAbility ability = bPlayer.getBoundAbility();
 		if (ability == null || !ability.isSneakAbility()) {
 			if (PassiveManager.hasPassive(player, CoreAbility.getAbility(FerroControl.class))) {
